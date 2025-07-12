7f5120a27e5baf7fc62189c61dd053fe7493b555
Anugrah James <anugrahjames2006@gmail.com>
2025-07-12 11:13:48 +0530

    Assignment 1 finished and 2 started

---
+++ 5.py
@@ -0,0 +1,18 @@
+print(10!=10)
+
+#Comparison operators
+
+a,b=15,10
+print(a==b)  # Equal to
+c,d=15,15
+print(c==d)  # Equal to
+x,y=15,10
+print(x!=y)  # Not equal to
+p,q=5,5
+print(p!=q)  # Not equal to
+r,s=3,4
+print(r>s)   # Greater than
+print(r<s)   # Less than
+m,n=30,30
+print(m>=n)  # Greater than or equal to
+print(m<=n)  # Less than or equal to
---
+++ 6.py
@@ -0,0 +1,12 @@
+print(10==10 or 15>20)
+print(not(10<20))
+
+#Logical Operators
+
+a=10
+print(a==10 and a<15)
+print(a==10 and a>15)
+print(a==10 or a>15)
+print(not(a==10))
+print(not(a>10))
+print(not(a<20))
